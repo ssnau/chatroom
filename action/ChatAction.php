cc20b49a95a2b9885d2da3099d294694bb3af09e
korige <korige@gmail.com>
2012-09-26 18:24:33 -0700

    first

---
+++ action/ChatAction.php
@@ -0,0 +1,9 @@
+<?php
+	/*
+	 * 聊天管理Action
+	 */
+	class ChatAction extends ActionClass {
+		public static function actDefault(){
+            self::display();
+		}
+    }
---
+++ template/chat/actDefault.php
@@ -0,0 +1,26 @@
+<?php include(ROOT_DIR. 'template/header.php'); ?>
+<div class='row'>
+<div class='span8'>
+        <!-- ChatRoom -->
+        <div class="accordion-group">
+            <div class="accordion-heading">
+                <h4 class="accordion-toggle" style='color:#999'> ChatRoom -  </h4>
+            </div><!-- ./accordion-heading -->
+            <div class="accordion-body in collapse" style="height: auto; ">
+            <div class="accordion-inner">
+            </div><!-- ./accordion-inner -->
+          </div><!-- ./accordion-body -->
+        </div><!-- ./accordion-group -->
+</div><!-- ./span8-->
+<div class='span4'>
+<!-- 登录框 -->
+<div class="accordion-group">
+	<div class="accordion-heading">
+		<h4 class="accordion-toggle" style='color:#999'> 当前成员 </h4>
+	</div><!-- ./accordion-heading -->
+	<div class="accordion-body in collapse" style="height: auto; ">
+	<div class="accordion-inner">
+	</div><!-- ./accordion-inner -->
+  </div><!-- ./accordion-body -->
+</div><!-- ./accordion-group -->
+<?php include(ROOT_DIR. 'template/footer.php'); ?>
